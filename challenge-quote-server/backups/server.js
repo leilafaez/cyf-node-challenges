7517d3aec2c1caad00c08fd5686af1f064eff45a
leilafaez <leila.faez@gmail.com>
2023-01-09 13:46:56 +0000

    Complete challenge quote-server

--- challenge-quote-server/backups/server.js
+++ challenge-quote-server/backups/server.js
@@ -2,12 +2,15 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
+const { response } = require("express");
 const express = require("express");
+const { request } = require("http");
 const app = express();
-
+const lodash = require("lodash");
+var cors = require("cors");
 //load the quotes JSON
 const Quotes = require("./quotes.json");
-
+app.use(cors());
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
@@ -17,7 +20,33 @@ app.get("/", function(request, response) {
 });
 
 //START OF YOUR CODE...
-
+app.get("/quotes",(request,response)=>{
+  response.json(Quotes)
+});
+app.get("/quotes/random",(request,response)=>{
+  response.json(pickFromArray(Quotes))
+});
+app.get("/quotes/search",(request,response)=>{
+  
+  if(request.query.term){
+    let result = Quotes.filter((item) => {
+      return (
+        item.quote.toLowerCase().includes(request.query.term.toLowerCase()) ||
+        item.author.toLowerCase().includes(request.query.term.toLowerCase())
+      );
+    });
+    response.send(result);
+  }
+  
+})
+app.get("/echo",(request,response)=>{
+  let word=request.query.word;
+  response.send(`you said ${word}`)
+})
+app.get("/quotes/random_quote",(request,response)=>{
+  const randQuote=lodash.sample(Quotes);
+  response.send(randQuote)
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
