7421734fb46d69637d2e176860965023fe6acae3
leilafaez <leila.faez@gmail.com>
2023-01-13 15:34:30 +0000

    Completer mailing list challenge

--- challenge-mailing-list/server.js
+++ challenge-mailing-list/server.js
@@ -2,7 +2,14 @@ const { response } = require("express");
 const express = require("express");
 const { request } = require("http");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 app.use(express.json());
+
+
+
+
 let lists= new Map();
 
 lists.set("staff", ["talea@techtonica.org", "michelle@techtonica.org"]);
@@ -30,8 +37,27 @@ app.get("/lists/:name",(request,response)=>{
   }
 })
 
-app.delete("lists/:name",(request,response)=>{
-  
+app.delete("/lists/:name",(request,response)=>{
+  const nameToDel = request.params.name;
+  const myList = Array.from(lists, ([name, members]) => ({ name, members }));
+  const result=myList.filter(item=>item.name !==nameToDel);
+  if(result){
+    response.status(200).json({result});
+  }else{
+    response.status(404).json();
+  }
+})
+app.put("/lists/:name",(request,response)=>{
+  const name= request.params.name;
+  const newName=request.body.name;
+  const myList = Array.from(lists, ([name, members]) => ({ name, members }));
+  const userIndex=myList.findIndex(user=>user.name===name);
+  if(userIndex===-1){
+    response.status(404).json(`user with name ${name} not found`)
+    return
+  } 
+  myList[userIndex].name=newName;
+  response.status(200).json({myList})
 })
 
 const listener = app.listen(process.env.PORT||3001, function () {
